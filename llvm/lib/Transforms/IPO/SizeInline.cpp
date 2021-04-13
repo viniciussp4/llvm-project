b9f6e62802a0705c9d09056793c7b3f80530f0b9
Rodrigo Rocha <rcor.cs@gmail.com>
2021-04-13 19:43:36 +0000

    added initial implementation for the trivial inlining.

--- llvm/lib/Transforms/IPO/SizeInline.cpp
+++ llvm/lib/Transforms/IPO/SizeInline.cpp
@@ -2,6 +2,7 @@
 #include "llvm/Transforms/IPO/SizeInline.h"
 
 #include "llvm/Transforms/IPO.h"
+#include "llvm/Transforms/Utils/Cloning.h"
 
 #include <vector>
 #include <algorithm>
@@ -19,8 +20,17 @@ static bool TrivialInlining(Module &M) {
     return F1->getInstructionCount() < F2->getInstructionCount();
   });
 
+  //from smallest to largest functions, perform trivial inlining
   for (Function *F : AllFunctions) {
-    errs() << F->getName() << ": " << F->getInstructionCount() << "\n";
+    if (F->getInstructionCount()<=2 || F->getNumUses()==1) {
+      errs() << "Trivially Inlining: " << F->getName() << "\n";
+      for (User *U : F->users()) {
+        if (CallBase *CB = dyn_cast<CallBase>(U)) {
+          InlineFunctionInfo IFI;
+	  auto Result = InlineFunction(*CB, IFI);
+	}
+      }
+    }
   }
 
   return false;
