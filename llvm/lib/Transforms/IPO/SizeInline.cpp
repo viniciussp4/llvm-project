3f6041562bc7726a1988f5af9ca2522e5c9e9aed
Vinicius Pacheco <vinicius-sp@hotmail.com>
2021-05-02 23:34:42 -0300

    vscode's auto indent, nothing new

--- llvm/lib/Transforms/IPO/SizeInline.cpp
+++ llvm/lib/Transforms/IPO/SizeInline.cpp
@@ -8,47 +8,50 @@
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace llvm;
 
-static cl::opt<unsigned> TinyThreshold (
-    "size-inline-tiny-threshold", cl::Hidden,
-    cl::desc("Always inine tiny functions."),
-    cl::init(2));
+static cl::opt<unsigned> TinyThreshold("size-inline-tiny-threshold", cl::Hidden,
+                                       cl::desc("Always inine tiny functions."),
+                                       cl::init(2));
 
-static cl::opt<unsigned> SolitaryMaxThreshold (
+static cl::opt<unsigned> SolitaryMaxThreshold(
     "size-inline-solitary-max-threshold", cl::Hidden,
     cl::desc("Limit the size of functions inlined due to solitary calls."),
     cl::init(1000));
 
-static cl::opt<unsigned> TrivialNumAttempts (
-    "size-inline-trivial-attempts", cl::Hidden,
-    cl::desc("Iteratively perform the trivial inlining."),
-    cl::init(5));
+static cl::opt<unsigned>
+    TrivialNumAttempts("size-inline-trivial-attempts", cl::Hidden,
+                       cl::desc("Iteratively perform the trivial inlining."),
+                       cl::init(5));
 
 static bool TrivialInlining(Module &M) {
-  std::vector<Function*> AllFunctions;
+  std::vector<Function *> AllFunctions;
   for (Function &F : M) {
-    if (F.isDeclaration()) continue;
+    if (F.isDeclaration())
+      continue;
     AllFunctions.push_back(&F);
   }
 
-  std::sort(AllFunctions.begin(), AllFunctions.end(), [&](const Function *F1, const Function *F2) -> bool {
-    return F1->getInstructionCount() < F2->getInstructionCount();
-  });
+  std::sort(AllFunctions.begin(), AllFunctions.end(),
+            [&](const Function *F1, const Function *F2) -> bool {
+              return F1->getInstructionCount() < F2->getInstructionCount();
+            });
 
   unsigned MaxSize = std::max(TinyThreshold, SolitaryMaxThreshold);
 
   bool Changed = false;
-  //from smallest to largest functions, perform trivial inlining
+  // from smallest to largest functions, perform trivial inlining
   for (Function *F : AllFunctions) {
     size_t FSize = F->getInstructionCount();
-    if (FSize > MaxSize) break; //functions are sorted
+    if (FSize > MaxSize)
+      break; // functions are sorted
 
     if (F->getNumUses() > 0 &&
-        (FSize<=TinyThreshold || (F->getNumUses()==1 && FSize<=SolitaryMaxThreshold))) {
+        (FSize <= TinyThreshold ||
+         (F->getNumUses() == 1 && FSize <= SolitaryMaxThreshold))) {
       errs() << "Trivially Inlining: " << F->getName() << "\n";
 
       auto it = F->users().begin();
@@ -61,8 +64,9 @@ static bool TrivialInlining(Module &M) {
           Changed = Changed || Result.isSuccess();
         }
       }
-      //aggressive: erase if unused after inlining
-      if (F->getNumUses()==0) F->eraseFromParent();
+      // aggressive: erase if unused after inlining
+      if (F->getNumUses() == 0)
+        F->eraseFromParent();
     }
   }
 
@@ -71,22 +75,21 @@ static bool TrivialInlining(Module &M) {
 
 static bool SituationalInlining(Module &M) {
   CallGraph CG(M);
-  //TODO: create SCC-based graph; navigate over the SCCs of this graph
-  //try to perform inlining within these SCCs.
+  // TODO: create SCC-based graph; navigate over the SCCs of this graph
+  // try to perform inlining within these SCCs.
   return true;
 }
 
-
 bool SizeInlining::runOnModule(Module &M) {
   bool Changed = false;
-  for (unsigned i = 0; i<TrivialNumAttempts; i++) {
+  for (unsigned i = 0; i < TrivialNumAttempts; i++) {
     bool AnyInlining = TrivialInlining(M);
     Changed = Changed || AnyInlining;
-    if (!AnyInlining) break;
+    if (!AnyInlining)
+      break;
   }
   return Changed;
 }
-
 
 void SizeInlining::getAnalysisUsage(AnalysisUsage &AU) const {
   ModulePass::getAnalysisUsage(AU);
@@ -96,6 +99,4 @@ char SizeInlining::ID = 0;
 INITIALIZE_PASS(SizeInlining, "size-inline", "New Inlining for Size", false,
                 false)
 
-ModulePass *llvm::createSizeInliningPass() {
-  return new SizeInlining();
-}
+ModulePass *llvm::createSizeInliningPass() { return new SizeInlining(); }
